231a8dc7e3570422b3ae3540e1f49743d6c69a25
qiu <zhengshen@nextbillion.ai>
2025-04-30 11:05:15 +0800

    Adapter flutter 3.24 for flutter lint

--- lib/src/color_tools.dart
+++ lib/src/color_tools.dart
@@ -3,9 +3,16 @@ part of "../nb_maps_flutter.dart";
 extension NbMapColorConversion on Color {
 
   String toHexStringRGB() {
-    final r = (this.r * 255).round().toRadixString(16).padLeft(2, '0');
-    final g = (this.g * 255).round().toRadixString(16).padLeft(2, '0');
-    final b = (this.b * 255).round().toRadixString(16).padLeft(2, '0');
+    // final r = (this.r * 255).round().toRadixString(16).padLeft(2, '0');
+    // final g = (this.g * 255).round().toRadixString(16).padLeft(2, '0');
+    // final b = (this.b * 255).round().toRadixString(16).padLeft(2, '0');
+    
+    // ignore: deprecated_member_use
+    final r = red.toRadixString(16).padLeft(2, '0');
+    // ignore: deprecated_member_use
+    final g = green.toRadixString(16).padLeft(2, '0');
+    // ignore: deprecated_member_use
+    final b = blue.toRadixString(16).padLeft(2, '0');
     return '#$r$g$b';
   }
 }
