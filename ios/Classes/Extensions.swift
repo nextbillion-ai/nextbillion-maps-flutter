73713b2370cf955b3daba5f5a7309e6785c74c7e
qiu <zhengshen@nextbillion.ai>
2025-05-07 13:30:00 +0800

    Add lint for iOS native codes

--- ios/Classes/Extensions.swift
+++ ios/Classes/Extensions.swift
@@ -94,8 +94,7 @@ extension UIImage {
         if let path = Bundle.main.path(forResource: absolutePath, ofType: nil) {
             let imageUrl = URL(fileURLWithPath: path)
             if let imageData: Data = try? Data(contentsOf: imageUrl),
-               let image = UIImage(data: imageData, scale: scale)
-            {
+               let image = UIImage(data: imageData, scale: scale) {
                 return image
             }
         }
@@ -105,7 +104,7 @@ extension UIImage {
 
 public extension UIColor {
     convenience init?(hexString: String) {
-        let r, g, b, a: CGFloat
+        let red, green, blue, alpha: CGFloat
 
         if hexString.hasPrefix("#") {
             let start = hexString.index(hexString.startIndex, offsetBy: 1)
@@ -116,22 +115,22 @@ public extension UIColor {
 
             if hexColor.count == 6 {
                 if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xFF0000) >> 16) / 255
-                    g = CGFloat((hexNumber & 0x00FF00) >> 8) / 255
-                    b = CGFloat(hexNumber & 0x0000FF) / 255
-                    a = 255
+                    red = CGFloat((hexNumber & 0xFF0000) >> 16) / 255
+                    green = CGFloat((hexNumber & 0x00FF00) >> 8) / 255
+                    blue = CGFloat(hexNumber & 0x0000FF) / 255
+                    alpha = 255
 
-                    self.init(red: r, green: g, blue: b, alpha: a)
+                    self.init(red: red, green: green, blue: blue, alpha: alpha)
                     return
                 }
             } else if hexColor.count == 8 {
                 if scanner.scanHexInt64(&hexNumber) {
-                    a = CGFloat((hexNumber & 0xFF00_0000) >> 24) / 255
-                    r = CGFloat((hexNumber & 0x00FF_0000) >> 16) / 255
-                    g = CGFloat((hexNumber & 0x0000_FF00) >> 8) / 255
-                    b = CGFloat(hexNumber & 0x0000_00FF) / 255
+                    alpha = CGFloat((hexNumber & 0xFF00_0000) >> 24) / 255
+                    red = CGFloat((hexNumber & 0x00FF_0000) >> 16) / 255
+                    green = CGFloat((hexNumber & 0x0000_FF00) >> 8) / 255
+                    blue = CGFloat(hexNumber & 0x0000_00FF) / 255
 
-                    self.init(red: r, green: g, blue: b, alpha: a)
+                    self.init(red: red, green: green, blue: blue, alpha: alpha)
                     return
                 }
             }
